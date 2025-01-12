a228f64745c2dcf85e499251e764b2edd37ed047
NeilH1910 <neilhinrichs@aol.com>
2025-01-12 19:16:20 +0100

    Roberta update

---
+++ src/model/roberta.py
@@ -0,0 +1,186 @@
+import math
+import torch
+from torch.utils.data import DataLoader, RandomSampler
+from torch import nn, optim
+from transformers import RobertaForSequenceClassification, AutoTokenizer
+import time
+
+
+tokenizer = AutoTokenizer.from_pretrained("FacebookAI/roberta-base")
+
+device = torch.device('cuda' if torch.cuda.is_available else 'cpu')
+
+homedir = "C:/Users/Neil/Documents/Uni/Master/BKIM"
+
+print("Start Data transformation")
+
+dummydata = [
+    ["Cold, Sneeze, Cough", [1, 2, 3]],
+    ["Stomach Hurt, Headache", [4, 5, 6]],
+    ["Low blood pressure, high heartbeatrate", [2, 5, 8]],
+    ["Fainting, low oxygen", [1, 8, 9]],
+    ["Hurting leg, fainting, high thirst", [4, 7, 8]],
+    ["Nausea, Vomiting", [0, 6, 9]],
+    ["High fever, muscle pain", [2, 4, 9]],
+    ["Chest pain, shortness of breath", [1, 5, 7]],
+    ["Dizziness, lightheadedness", [3, 6, 8]],
+    ["Joint pain, fatigue", [5, 6, 7]],
+    ["Rash, itching", [4, 8, 9]],
+    ["Earache, fever", [1, 2, 5]],
+    ["Back pain, discomfort", [3, 4, 6]],
+    ["Dry throat, difficulty swallowing", [2, 5, 9]],
+    ["Weight loss, increased thirst", [1, 7, 8]],
+    ["Mood swings, insomnia", [3, 5, 9]],
+    ["Sore throat, fatigue", [2, 6, 9]],
+    ["Swelling, bruising", [1, 3, 8]],
+    ["Blurred vision, headache", [4, 5, 9]],
+    ["Cold extremities, shivering", [2, 6, 8]]
+]
+dummydata = dummydata *50
+print(len(dummydata))
+
+dummyset=[]
+for data in dummydata:
+    inp = tokenizer(data[0],padding='max_length', max_length=50)
+    outp = data[1]
+    a = [*[torch.tensor(v).to(device) for k,v in inp.items()], torch.tensor(outp).to(device)]
+    dummyset.append(a)
+
+trainsize = int(len(dummyset)*0.8)
+testsize = len(dummyset) - trainsize
+traindata, evaldata = torch.utils.data.random_split(dummyset,[trainsize,testsize])
+print(len(traindata))
+
+batch_size = 128
+
+train_sampler = RandomSampler(traindata)
+train_dataloader = DataLoader(traindata, sampler=train_sampler, batch_size=batch_size)
+eval_sampler = RandomSampler(evaldata)
+eval_dataloader = DataLoader(evaldata, sampler=eval_sampler, batch_size=batch_size)
+
+
+class CustomRobertaForSequenceClassification(nn.Module):
+    def __init__(self, pretrained_model_name='FacebookAI/roberta-base', num_labels=10):
+        super(CustomRobertaForSequenceClassification, self).__init__()
+        self.roberta = RobertaForSequenceClassification.from_pretrained(pretrained_model_name,num_labels=num_labels)
+        self.num_labels = num_labels
+
+    def forward(self, input_ids, attention_mask=None, token_type_ids=None):
+        outputs = self.roberta(input_ids=input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids)
+
+        return outputs.logits
+
+def asMinutes(s):
+    m = math.floor(s / 60)
+    s -= m * 60
+    return '%dm %ds' % (m, s)
+
+def timeSince(since, percent):
+    now = time.time()
+    s = now - since
+    es = s / (percent)
+    rs = es - s
+    return '%s (- %s)' % (asMinutes(s), asMinutes(rs))
+    
+model = CustomRobertaForSequenceClassification(num_labels=20000).to(device)
+
+def train_epoch(dataloader, encoder, optimizer, criterion):
+    total_loss = 0
+    
+    for data in dataloader:
+        inp, att, out = data
+
+        optimizer.zero_grad()
+
+        logits = encoder(inp, attention_mask = att)
+
+        targets = torch.zeros(logits.size(), dtype=torch.float32).to(device)
+        targets[0, out] = 1.0
+        loss = criterion(logits, targets)
+        loss.backward()
+
+        optimizer.step()
+
+        total_loss += loss.item()
+
+        
+    return total_loss / len(dataloader)
+
+def test_epoch(dataloader, encoder,  criterion):
+    total_loss = 0
+
+    for data in dataloader:
+        inp, att, out = data
+
+        logits = encoder(inp, attention_mask = att)
+
+        targets = torch.zeros(logits.size(), dtype=torch.float32).to(device)
+        targets[0, out] = 1.0
+        loss = criterion(logits, targets)
+
+        total_loss += loss.item()
+        
+    return total_loss / len(dataloader)
+
+def train(train_dataloader, eval_dataloader, encoder, epochs, lr = 0.0001, print_every=1):
+    print_loss_total = 0
+    test_loss_total = 0
+    min_loss = 100
+    
+    encoder_optimizer = optim.Adam(encoder.parameters(), lr=lr)
+
+    _ ,_, fb = next(iter(train_dataloader))
+    pos_weight = torch.tensor([(encoder.num_labels - len(fb[0])) / len(fb[0])]).to(device)
+    criterion = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
+    encoder.train()
+
+    start = time.time()
+
+    for epoch in range(1, epochs+1):
+        loss = train_epoch(train_dataloader, encoder, encoder_optimizer,criterion)
+        test_loss = test_epoch(eval_dataloader,encoder,criterion)
+
+        print_loss_total += loss
+        test_loss_total += test_loss
+
+        if epoch % print_every == 0:
+            print_loss_avg = print_loss_total / print_every
+            print_loss_total = 0
+            test_loss_avg = test_loss_total / print_every
+            test_loss_total = 0
+            print('%s (%d %d%%) %.4f' % (timeSince(start, epoch / epochs),
+                                        epoch, epoch / epochs * 100, print_loss_avg))
+
+        if test_loss_avg < min_loss:
+            min_loss = test_loss_avg
+            torch.save(model.state_dict(), homedir+'/llmroberta.pth')
+            print("Test Loss: " + str(test_loss_avg) + "\n")
+
+    torch.save(model.state_dict(), homedir+'/llmrobertalaststate.pth')
+
+        
+
+print("Start Training")
+train(train_dataloader,eval_dataloader,model,10, lr = 0.001)
+
+
+
+def eval(input):
+    model.eval()
+
+    input = tokenizer(input,padding='max_length', max_length=50)
+
+    inp = torch.tensor(input['input_ids']).to(device).unsqueeze(0)
+    att = torch.tensor(input['attention_mask']).to(device).unsqueeze(0)
+
+    out = model(inp, attention_mask = att)
+
+    out = torch.sigmoid(out)
+    #print('Probs: ', out)
+    out = out > 0.8
+    out = torch.nonzero(out, as_tuple=True)
+
+    papers = out[1].tolist()
+    print(papers)
+
+eval("Cold, Sneeze, Cough")
