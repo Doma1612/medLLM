307ea1022d4e3be5e5551cd824e1f49545a76dd3
Dominik Martens <Dominik.Martens@outlook.com>
2025-01-16 13:00:52 +0100

    implement pdf extraction and saving

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from data.data_transformer import main
+
+
+# IMPORTANT 
+"""Please change the csv paths for in and output files in the main function
+   Also: Change the header in the request. Getting your agent:
+   Go to browser, rightclick, inspect, go to network tab, reload a site and click on any request. Scroll down in the headers section
+   until you find the User-Agent value.
+"""
+# get access to pfs and save in csv's:
+main()
